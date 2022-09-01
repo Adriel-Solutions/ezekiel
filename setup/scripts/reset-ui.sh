89fdfa21fe4f3b202c4c94b7477f4b012c6d1c29
David - Adriel <david@adriel.solutions>
2022-09-01 12:19:04 +0300

    Fixed script to reset colors

--- setup/scripts/reset-ui.sh
+++ setup/scripts/reset-ui.sh
@@ -1,9 +1,10 @@
+#!/bin/bash
 read -p "Initial color : " initial_color
 read -p "New color : " new_color
 
-find app/views/layouts -type f -name "*.php" -exec sed -i '' -E 's/teal/blue/g' {} \;
-find app/views/partials -type f -name "*.php" -exec sed -i '' -E 's/teal/blue/g' {} \;
-find app/views/components -type f -name "*.php" -exec sed -i '' -E 's/teal/blue/g' {} \;
-find app/views/emails -type f -name "*.php" -exec sed -i '' -E 's/teal/blue/g' {} \;
-find app/views/pages -type f -name "*.php" -exec sed -i '' -E 's/teal/blue/g' {} \;
-find app/views/skeletons -type f -name "*.php" -exec sed -i '' -E 's/teal/blue/g' {} \;
+find app/views/layouts -type f -name "*.php" -exec sed -i '' -E "s/$initial_color/$new_color/g" {} \;
+find app/views/partials -type f -name "*.php" -exec sed -i '' -E "s/$initial_color/$new_color/g" {} \;
+find app/views/components -type f -name "*.php" -exec sed -i '' -E "s/$initial_color/$new_color/g" {} \;
+find app/views/emails -type f -name "*.php" -exec sed -i '' -E "s/$initial_color/$new_color/g" {} \;
+find app/views/pages -type f -name "*.php" -exec sed -i '' -E "s/$initial_color/$new_color/g" {} \;
+find app/views/skeletons -type f -name "*.php" -exec sed -i '' -E "s/$initial_color/$new_color/g" {} \;
