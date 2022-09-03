cf20ae39717f688f6e066790a86eb35a95fb93d0
David - Adriel <david@adriel.solutions>
2022-09-03 18:19:47 +0300

    Introducted collections for better type-hinting

---
+++ native/collections/adapters.php
@@ -0,0 +1,33 @@
+<?php
+
+    namespace native\collections;
+
+    use ArrayAccess;
+    use native\libs\Adapter;
+
+    class Adapters implements ArrayAccess {
+        private array $data = [];
+
+        public function offsetGet(mixed $offset) : ?Adapter 
+        {
+            return $this->data[$offset] ?? null;
+        }
+
+        public function offsetSet(mixed $offset, mixed $value) : void 
+        {
+            if (is_null($offset)) 
+                $this->data[] = $value;
+             else 
+                $this->data[$offset] = $value;
+        }
+
+        public function offsetExists($offset) : bool 
+        {
+            return isset($this->data[$offset]);
+        }
+
+        public function offsetUnset(mixed $offset) : void 
+        {
+            unset($this->data[$offset]);
+        }
+    }
