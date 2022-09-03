32202184fda9409ad23ac223927eed7bb6284ce3
David - Adriel <david@adriel.solutions>
2022-09-03 18:29:26 +0300

    Added support for auto-renaming containers randomly to prevent docker conflicts

--- setup/scripts/run.sh
+++ setup/scripts/run.sh
@@ -1,6 +1,26 @@
+# Shutdown the containers
 docker-compose -f docker-compose.dev.yml down
+
+# Generate new names for the containers
+random_prefix="$(echo $RANDOM | base64 | head -c 20 | tr -d '=')"
+containers=""
+containers="${containers} db"
+containers="${containers} web"
+containers="${containers} fpm"
+
+for container in ${containers}; do
+    # container_name: "xxx-db"
+    sed -i '' -E "s/\".+-$container\"/\"$random_prefix-$container\"/g" docker-compose.dev.yml
+
+    # depends_on:\n- xxx-db
+    # sed -i '' -E "s/- .+-$container/- $random_prefix-$container/g" docker-compose.dev.yml
+done
+
+# Restart everything brand new
 docker-compose -f docker-compose.dev.yml up -d
+
+# Set up database and dependencies
 ./setup/scripts/install-dependencies.sh
-./setup/scripts/create-new-env.sh
 ./setup/scripts/reset-db.sh
 ./setup/scripts/run-db-migrations.sh
+
