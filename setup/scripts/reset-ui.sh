d55ba39a69923397ea01f5dc18726b4c72aac9a1
David - Adriel <david@adriel.solutions>
2022-09-03 18:28:51 +0300

    Refactored to prevent harming the source code and properly edit modules as well

--- setup/scripts/reset-ui.sh
+++ setup/scripts/reset-ui.sh
@@ -2,9 +2,15 @@
 read -p "Initial color : " initial_color
 read -p "New color : " new_color
 
-find app/views/layouts -type f -name "*.php" -exec sed -i '' -E "s/$initial_color/$new_color/g" {} \;
-find app/views/partials -type f -name "*.php" -exec sed -i '' -E "s/$initial_color/$new_color/g" {} \;
-find app/views/components -type f -name "*.php" -exec sed -i '' -E "s/$initial_color/$new_color/g" {} \;
-find app/views/emails -type f -name "*.php" -exec sed -i '' -E "s/$initial_color/$new_color/g" {} \;
-find app/views/pages -type f -name "*.php" -exec sed -i '' -E "s/$initial_color/$new_color/g" {} \;
-find app/views/skeletons -type f -name "*.php" -exec sed -i '' -E "s/$initial_color/$new_color/g" {} \;
+DIRS=""
+DIRS="${DIRS} app/views/layouts"
+DIRS="${DIRS} app/views/partials"
+DIRS="${DIRS} app/views/components"
+DIRS="${DIRS} app/views/emails"
+DIRS="${DIRS} app/views/pages"
+DIRS="${DIRS} app/views/skeletons"
+DIRS="${DIRS} app/modules/**/views"
+
+for SRC in ${DIRS}; do
+    find $SRC -type f -name "*.php" -exec sed -i '' -E "s/-$initial_color-/-$new_color-/g" {} \;
+done
