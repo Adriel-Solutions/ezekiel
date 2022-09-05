7ef419cbcaad54d2689e51ccaba98c7310e1f5bd
David - Adriel <david@adriel.solutions>
2022-09-05 11:14:57 +0300

    Added support for not renaming containers via a cli switch

--- setup/scripts/run.sh
+++ setup/scripts/run.sh
@@ -1,20 +1,22 @@
 # Shutdown the containers
 docker-compose -f docker-compose.dev.yml down
 
-# Generate new names for the containers
-random_prefix="$(echo $RANDOM | base64 | head -c 20 | tr -d '=')"
-containers=""
-containers="${containers} db"
-containers="${containers} web"
-containers="${containers} fpm"
+if [[ -z $1 || $1 != "--no-random" ]]; then
+    # Generate new names for the containers
+    random_prefix="$(echo $RANDOM | base64 | head -c 20 | tr -d '=')"
+    containers=""
+    containers="${containers} db"
+    containers="${containers} web"
+    containers="${containers} fpm"
 
-for container in ${containers}; do
-    # container_name: "xxx-db"
-    sed -i '' -E "s/\".+-$container\"/\"$random_prefix-$container\"/g" docker-compose.dev.yml
+    for container in ${containers}; do
+        # container_name: "xxx-db"
+        sed -i '' -E "s/\".+-$container\"/\"$random_prefix-$container\"/g" docker-compose.dev.yml
 
-    # depends_on:\n- xxx-db
-    # sed -i '' -E "s/- .+-$container/- $random_prefix-$container/g" docker-compose.dev.yml
-done
+        # depends_on:\n- xxx-db
+        # sed -i '' -E "s/- .+-$container/- $random_prefix-$container/g" docker-compose.dev.yml
+    done
+fi
 
 # Restart everything brand new
 docker-compose -f docker-compose.dev.yml up -d
