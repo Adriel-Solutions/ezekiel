7a693f693bf6c36095490aadee944787dde87753
David - Adriel <david@adriel.solutions>
2022-09-10 19:03:28 +0300

    Updated dependency install script + Added ezekiel cli as a dependency

--- setup/scripts/install-dependency.sh
+++ setup/scripts/install-dependency.sh
@@ -1,2 +1,2 @@
 name=$(cat docker-compose.dev.yml | grep -E "container_name: .*-fpm" | sed -E 's/container_name: "(.+)"$/\1/' | tr -d ' ')
-docker exec -it $name composer require $1
+docker exec -it $name composer require $@
