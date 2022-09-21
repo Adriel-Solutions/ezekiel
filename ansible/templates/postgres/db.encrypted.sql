f61f7f7a021519dce8d51ce2b96748747beff994
David - Adriel <david@adriel.solutions>
2022-09-21 15:46:54 +0200

    Fixed yml extensions for Ansible and improved deployment script

---
+++ ansible/templates/postgres/db.encrypted.sql
@@ -0,0 +1,41 @@
+-- default database
+\c project ezekiel
+
+-- updated_at refresh function (to be called inside a trigger)
+CREATE OR REPLACE FUNCTION refresh_updated_at()
+RETURNS TRIGGER AS $$
+BEGIN
+  NEW.updated_at = NOW();
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+-- migrations
+CREATE TABLE migrations (
+    pk BYTEA NOT NULL,
+    name BYTEA NOT NULL,
+
+    PRIMARY KEY (pk)
+);
+
+-- jobs
+CREATE TABLE jobs (
+    pk BYTEA NOT NULL,
+    class BYTEA NOT NULL,
+
+    is_running BYTEA NOT NULL,
+    is_unique BYTEA NOT NULL,
+    context BYTEA NOT NULL,
+    report BYTEA NULL DEFAULT NULL,
+    progress BYTEA NULL DEFAULT NULL,
+
+    scheduled_for BYTEA NULL DEFAULT NULL,
+    schedule_frequency BYTEA NULL DEFAULT NULL,
+    schedule_unit BYTEA NULL DEFAULT NULL,
+
+    last_run_at BYTEA NULL DEFAULT NULL,
+    created_at BYTEA NOT NULL,
+
+    PRIMARY KEY (pk)
+);
+
