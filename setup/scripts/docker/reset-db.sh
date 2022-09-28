107d4db3c8ef360abab0f69b3af756bfd738b902
David - Adriel <david@adriel.solutions>
2022-09-28 14:43:42 +0200

    Removed useless docker scripts in favor of CLI + Upgraded CLI

--- setup/scripts/docker/reset-db.sh
+++
@@ -1,3 +0,0 @@
-name=$(cat docker-compose.dev.yml | grep -E "container_name: .*-db" | sed -E 's/container_name: "(.+)"$/\1/' | tr -d ' ')
-docker exec -it --user postgres $name psql -c 'DROP DATABASE project;'
-docker exec -it --user postgres $name psql -f /docker-entrypoint-initdb.d/init.sql
