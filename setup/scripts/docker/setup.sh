a8fc8e7559b9bc418aff40987e2e25df5e94503d
David - Adriel <david@adriel.solutions>
2022-09-28 14:17:25 +0200

    Removed useless step

--- setup/scripts/docker/setup.sh
+++ setup/scripts/docker/setup.sh
@@ -1,5 +1,3 @@
-docker-compose -f docker-compose.dev.yml down
-
 if [[ -z $1 || $1 != "--no-random" ]]; then
     # Generate new names for the containers
     random_prefix="$(echo $RANDOM | base64 | head -c 20 | tr -d '=')"
@@ -11,9 +9,6 @@ if [[ -z $1 || $1 != "--no-random" ]]; then
     for container in ${containers}; do
         # container_name: "ezekiel-xxx-db"
         sed -i '' -E "s/\".+-$container\"/\"ezekiel-$random_prefix-$container\"/g" docker-compose.dev.yml
-
-        # depends_on:\n- xxx-db
-        # sed -i '' -E "s/- .+-$container/- $random_prefix-$container/g" docker-compose.dev.yml
     done
 fi
 
