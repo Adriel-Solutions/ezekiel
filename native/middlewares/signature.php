c2562f43f2d797cde5176babbf14df2b25b7d78f
David - Adriel <david@adriel.solutions>
2022-09-28 14:56:56 +0200

    Quick refactoring

--- native/middlewares/signature.php
+++ native/middlewares/signature.php
@@ -7,21 +7,19 @@
         public function __invoke($req, $res, &$next) {
             if ( Options::get('ANTI_TAMPERING_ENABLED') !== true ) return;
 
-            if(empty($req->headers['x-adriel-signature'])) {
+            if( empty($req->headers['x-adriel-signature']) ) {
                 $next = false;
                 return $res->send_malformed();
             }
 
             $supplied_signature = $req->headers['x-adriel-signature'];   
+            $supplied_content = $req->method === 'GET' ? 'uri' : 'raw';
 
-            if($req->method === 'GET')
-                $computed_signature = base64_encode(hash_hmac('sha256', $req->uri, Options::get('SIGNATURE_SECRET'), true));
-            else
-                $computed_signature = base64_encode(hash_hmac('sha256', $req->raw, Options::get('SIGNATURE_SECRET'), true));
+            $computed_signature = base64_encode(hash_hmac('sha256', $req->$supplied_content, Options::get('SIGNATURE_SECRET'), true));
 
-            if($computed_signature !== $supplied_signature) {
-                $next = false;
-                return $res->send_malformed();
-            }
+            if( $computed_signature === $supplied_signature ) return;
+
+            $next = false;
+            return $res->send_malformed();
         }
     }
