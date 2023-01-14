d57b552cdc6b63171f6f34d311708587282e01f0
David - Adriel <david@adriel.solutions>
2023-01-14 19:18:38 +0100

    Added full cache invalidation mechanism, and updated dotenv sample

--- native/routers/api.php
+++ native/routers/api.php
@@ -3,10 +3,33 @@
 
     use native\libs\Router;
     use native\libs\Options;
+use native\libs\Request;
+use native\libs\Response;
 
     class Api extends Router {
         protected function load() {
             $this->set_prefix(Options::get('ROOT_API'));
+
+            // Default cache invalidation mechanism, when applicable
+            if(Options::get('CACHE_ENABLED'))
+                $this->get('/invalidate', function(Request $req, Response $res) {
+                    switch(true) {
+                        case empty($req->query):
+                        case empty($req->query['route']):
+                        case empty($req->query['secret']):
+                        case $req->query['secret'] !== Options::get('CACHE_SECRET'):
+                            return $res->send_malformed();
+                    }
+
+                    $route = str_replace([ '.' ], '',strtolower($req->query['route']));
+
+                    if(!file_exists(cache_path($route)))
+                        return $res->send_conflict();
+
+                    unlink(cache_path($route));
+
+                    $res->send_success();
+                });
 
             $this->mount(new \app\routers\Api());
 
